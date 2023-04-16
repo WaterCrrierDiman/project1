4bef68e5c5359edef2615fd29a226d1f42ec8e1c
Dmitriy Sulemov <sulemov0606@mail.ru>
2023-04-16 15:30:37 +0300

    Added file1

---
+++ 1.py
@@ -0,0 +1,12 @@
+from random import randint
+mass = [randint(0, 10) for s in range(10)]
+mx_1, mx_2 = mass[0], mass[1]
+if mass[0] < mass[1]:
+    mx_1, mx_2 = mx_2, mx_1
+for i in range(2, 10):
+    if mass[i] > mx_1:
+        mx_2, mx_1 = mx_1, mass[i]
+    elif mass[i] > mx_2 and mass[i] != mx_1:
+        mx_2 = mass[i]
+print(mass)
+print(mx_2)
